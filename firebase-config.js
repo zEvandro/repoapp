eb1584f3198c2c39ebb128c70649d0c4fa195438
Evandro Silva <evandro.rodrig.er@gmail.com>
2026-05-08 17:49:21 -0300

    adiciona firebase-config.js e atualiza gitignore para GitHub Pages

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Este arquivo NÃO vai para o repositório (está no .gitignore)
+// Copie firebase-config.example.js, preencha com suas credenciais e renomeie para firebase-config.js
+
+window.FIREBASE_CONFIG = {
+  apiKey:            "AIzaSyCp7oep4v4YTv9KjU_BCNAl5jgWlCTdvmI",
+  authDomain:        "repoapp-1edbd.firebaseapp.com",
+  databaseURL:       "https://repoapp-1edbd-default-rtdb.firebaseio.com",
+  projectId:         "repoapp-1edbd",
+  storageBucket:     "repoapp-1edbd.firebasestorage.app",
+  messagingSenderId: "151491743077",
+  appId:             "1:151491743077:web:06aa81e8ce4c4e032f258c"
+};
