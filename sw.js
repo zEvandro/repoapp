abf68a74a450afc4923d866577bbdd08da427acb
Evandro Silva <evandro.rodrig.er@gmail.com>
2026-05-08 22:20:23 -0300

    fix v3.27: presenca do operador com padrao .info/connected do Firebase
    - onDisconnect registrado ANTES do set (evita race condition)
    - .info/connected re-registra o handler automaticamente a cada reconexao
    - removerOperador() cancela o listener antes de remover do DB
    - Operador some da tela do repositor imediatamente ao sair

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "repoapp-v3.26";
+const CACHE = "repoapp-v3.27";
 const FILES = ["./", "./index.html", "./manifest.json", "./firebase-config.js"];
 
 self.addEventListener("install", e => {
